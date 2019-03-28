0cb3e926ea71ae5f463babd6c78b17c9029692ff
Pherpher089 <Christutor089@gmail.com>
2019-03-28 01:44:35 -0700

    Completed the call back assignment

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum','Gum'];
 
 /* 
 
@@ -26,24 +26,34 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  cb(arr.length);
 }
 
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 }
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x * y);
 }
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for(let i = 0; i < list.lenght; i++)
+  {
+    if(list[i] === item)
+    {
+      cb(true);
+      return;
+    }
+  }
+  cb(false);
 }
 
 /* STRETCH PROBLEM */
@@ -52,4 +62,25 @@ function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  const noDupes = array;
+
+  for(let i = 0; i < array.length; i++)
+  {
+    for(let j = 0; j < array.length; j++)
+    {
+      if(i !== j)
+      {
+        if(noDupes[i] === noDupes[j])
+        {
+          noDupes.splice(j, 1);
+        }
+      }
+    } 
+  }
+  cb(noDupes);
 }
+getLength(items, console.log);
+last(items, console.log);
+sumNums(10, 5, console.log);
+multiplyNums(3, 12, console.log);
+removeDuplicates(items, console.log);
